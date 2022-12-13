731ab1fa87f9e9474db5294718959dc9a8859afd
Derek Visch <dvisch@autoidm.com>
2022-12-13 15:46:58 -0500

    Make code a bit more extensible

--- tap_postgres/tests/test_core.py
+++ tap_postgres/tests/test_core.py
@@ -26,33 +26,43 @@ def test_standard_tap_tests():
     for test in tests:
         test()
 
-def test_replication_key():
-    #TODO pull out stuff here
-    fake = Faker()
+@pytest.fixture()
+def sqlalchemy_connection(tap):
+    #Used this function to avoid server side cursors which don't work with DDL
+    return tap.default_stream_class.connector_class(dict(tap.config)).create_sqlalchemy_engine().connect()
+
+@pytest.fixture()
+def tap():
+    return TapPostgres(config=SAMPLE_CONFIG)
+
+@pytest.fixture()
+def fake():
+    return Faker()
+
+def test_replication_key(sqlalchemy_connection, tap, fake):
+    """Originally built to address https://github.com/MeltanoLabs/tap-postgres/issues/9"""
     date1 = datetime.date(2022,12,1)
     date2 = datetime.date(2022,12,30)
-    fake.date_between(date1, date2)
-    pg_tap = TapPostgres(config=SAMPLE_CONFIG)
-    pg_connector = pg_tap.default_stream_class.connector_class(dict(pg_tap.config))
-    sqlalchemy_connection = pg_connector.create_sqlalchemy_engine().connect()
     metadata_obj = MetaData()
+    table_name = "test_replication_key"
 
     test_replication_key_table = Table(
-            "test_replication_key",
+            table_name,
             metadata_obj,
             Column("id", Integer, primary_key=True),
             Column("updated_at", DateTime(), nullable=False),
             Column("name", String())
         )
     metadata_obj.create_all(sqlalchemy_connection)
-    sqlalchemy_connection.execute(f"TRUNCATE TABLE {test_replication_key_table.name}")
+    sqlalchemy_connection.execute(f"TRUNCATE TABLE {table_name}")
 
-    for _ in range(1000):
+    for _ in range(10):
+        #TODO this is slow we could speed this up by bulk inserting
         insert = test_replication_key_table.insert().values(updated_at=fake.date_between(date1,date2), name=fake.name())
         sqlalchemy_connection.execute(insert)
 
     #Create sink for table
-    streams = pg_tap.discover_streams()
-    test_replication_key_stream = next(stream for stream in streams if stream.name.split("-")[1] == "test_replication_key")
+    streams = tap.discover_streams()
+    test_replication_key_stream = next(stream for stream in streams if stream.name.split("-")[1] == table_name)
     test_replication_key_stream.replication_key = "updated_at"
     test_replication_key_stream.sync()
