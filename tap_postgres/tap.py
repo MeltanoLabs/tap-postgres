bd1ab57e3358b26fe205c572def79129e9e901dd
Derek Visch <dvisch@autoidm.com>
2023-04-14 21:58:07 -0400

    SSH Tunnel working, testing to add, and private_key via config to add

--- tap_postgres/tap.py
+++ tap_postgres/tap.py
@@ -1,6 +1,8 @@
 """Postgres tap class."""
 from __future__ import annotations
 
+import atexit
+import signal
 from functools import cached_property
 from pathlib import PurePath
 from typing import List
@@ -17,96 +19,6 @@ from tap_postgres.client import PostgresConnector, PostgresStream
 
 class TapPostgres(SQLTap):
     """Postgres tap class."""
-
-    def _scary_load_config(
-        self,
-        config: dict | PurePath | str | list[PurePath | str] | None = None,
-        parse_env_config: bool = False,
-        validate_config: bool = True,
-    ) -> None:
-        """Stole from /singer_sdk/plugin_base.py __init__ method.
-
-        Need to load _config before calling super().__init__ so that we can use the config to setup the ssh tunnel.
-
-        Args:
-            config: May be one or more paths, either as str or PurePath objects, or
-                it can be a predetermined config dict.
-            parse_env_config: True to parse settings from env vars.
-            validate_config: True to require validation of config settings.
-
-        Raises:
-            ValueError: If config is not a dict or path string.
-        """
-        if not config:
-            config_dict = {}
-        elif isinstance(config, str) or isinstance(config, PurePath):
-            config_dict = read_json_file(config)
-        elif isinstance(config, list):
-            config_dict = {}
-            for config_path in config:
-                # Read each config file sequentially. Settings from files later in the
-                # list will override those of earlier ones.
-                config_dict.update(read_json_file(config_path))
-        elif isinstance(config, dict):
-            config_dict = config
-        else:
-            raise ValueError(f"Error parsing config of type '{type(config).__name__}'.")
-        if parse_env_config:
-            self.logger.info("Parsing env var for settings config...")
-            config_dict.update(self._env_var_config)
-        else:
-            self.logger.info("Skipping parse of env var settings...")
-        for k, v in config_dict.items():
-            if self._is_secret_config(k):
-                config_dict[k] = SecretString(v)
-        self._config = config_dict
-        self._validate_config(raise_errors=validate_config)
-
-    def __init__(
-        self,
-        config: dict | PurePath | str | list[PurePath | str] | None = None,
-        catalog: PurePath | str | dict | None = None,
-        state: PurePath | str | dict | None = None,
-        parse_env_config: bool = False,
-        validate_config: bool = True,
-    ) -> None:
-        """Initialize the SQL tap.
-
-        See super() for details, this constructor is here for the ssh tunnel connection and teardown
-        """
-        self._scary_load_config(
-            config=config,
-            parse_env_config=parse_env_config,
-            validate_config=validate_config,
-        )
-        self.url = make_url(
-            self.config["sqlalchemy_url"]
-        )  # We mutate this url to use the ssh tunnel if enabled
-        if self.config["ssh_tunnel.enable"]:
-            self.ssh_tunnel: SSHTunnelForwarder = SSHTunnelForwarder(
-                ssh_address_or_host=(
-                    self.config["ssh_tunnel.host"],
-                    self.config["ssh_tunnel.port"],
-                ),
-                ssh_username=self.config["ssh_tunnel.username"],
-                # TODO Update this to use the private_key from env instead of file
-                ssh_private_key=self.config["ssh_tunnel.private_key"],
-                ssh_private_key_password=self.config["ssh_tunnel.private_key_password"],
-                remote_bind_address=(self.url.host, self.url.port),
-            )
-            new_host = self.ssh_tunnel.local_bind_address
-            new_port = self.ssh_tunnel.local_bind_port
-
-            self.url.host = new_host
-            self.url.port = new_port
-
-        super().__init__(
-            config=config,
-            catalog=catalog,
-            state=state,
-            parse_env_config=parse_env_config,
-            validate_config=validate_config,
-        )
 
     name = "tap-postgres"
     default_stream_class = PostgresStream
@@ -169,12 +81,67 @@ class TapPostgres(SQLTap):
         Connector is a singleton (one instance is used by the Tap and Streams).
 
         """
+        # We mutate this url to use the ssh tunnel if enabled
+        self.url = make_url(self.config["sqlalchemy_url"])
+
+        if self.config["ssh_tunnel.enable"]:
+            self.ssh_tunnel_connect()
         return PostgresConnector(
             config=dict(self.config),
             sqlalchemy_url=str(
                 self.url
             ),  # Sometimes we mutuate the url to use the ssh tunnel
         )
+
+    def ssh_tunnel_connect(self) -> None:
+        self.ssh_tunnel: SSHTunnelForwarder = SSHTunnelForwarder(
+            ssh_address_or_host=(
+                self.config["ssh_tunnel.host"],
+                self.config["ssh_tunnel.port"],
+            ),
+            ssh_username=self.config["ssh_tunnel.username"],
+            # TODO Update this to use the private_key from env instead of file
+            ssh_private_key=self.config["ssh_tunnel.private_key"],
+            ssh_private_key_password=self.config.get("ssh_tunnel.private_key_password"),
+            remote_bind_address=(self.url.host, self.url.port),
+        )
+        self.ssh_tunnel.start()
+        self.logger.info("SSH Tunnel started")
+        # On program exit clean up, want to also catch signals
+        atexit.register(self.clean_up)
+        signal.signal(signal.SIGTERM, self.catch_signal)
+        signal.signal(signal.SIGINT, self.catch_signal)
+
+        #  Swap URL and Port to use tunnel
+        self.url = self.url.set(
+            host=self.ssh_tunnel.local_bind_host, port=self.ssh_tunnel.local_bind_port
+        )
+
+    def clean_up(self):
+        self.logger.info("Shutting down SSH Tunnel")
+        self.ssh_tunnel.stop()
+
+    def catch_signal(self, signum, frame):
+        exit(1)  # Be sure atexit is called, so clean_up gets called
+
+    @property
+    def catalog_dict(self) -> dict:
+        """Get catalog dictionary.
+
+        Returns:
+            The tap's catalog as a dict
+        """
+        if self._catalog_dict:  # type: ignore
+            return self._catalog_dict  # type: ignore
+
+        if self.input_catalog:
+            return self.input_catalog.to_dict()
+
+        result: dict[str, list[dict]] = {"streams": []}
+        result["streams"].extend(self.connector.discover_catalog_entries())
+
+        self._catalog_dict = result
+        return self._catalog_dict
 
     def discover_streams(self) -> List[Stream]:
         """Initialize all available streams and return them as a list.
