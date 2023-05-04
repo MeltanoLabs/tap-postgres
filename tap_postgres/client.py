3a93f1efd0c6670d4b35da9fa48ab88b2034de89
Jeffrey Im <jim@keybank.com>
2023-05-04 18:28:48 -0400

    adding back JSONB conformance level

--- tap_postgres/client.py
+++ tap_postgres/client.py
@@ -9,6 +9,7 @@ from typing import TYPE_CHECKING, Any, Dict, Iterable, Optional, Type, Union
 import sqlalchemy
 from singer_sdk import SQLConnector, SQLStream
 from singer_sdk import typing as th
+from singer_sdk.helpers._typing import TypeConformanceLevel
 
 if TYPE_CHECKING:
     from sqlalchemy.dialects import postgresql
@@ -146,6 +147,9 @@ class PostgresStream(SQLStream):
     """Stream class for Postgres streams."""
 
     connector_class = PostgresConnector
+
+    # JSONB Objects won't be selected without type_confomance_level to ROOT_ONLY
+    TYPE_CONFORMANCE_LEVEL = TypeConformanceLevel.ROOT_ONLY
 
     def get_query(self, context: Optional[dict] = None) -> Any:
         """Return a query with selected columns
