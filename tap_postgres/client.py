12971d354da49c0a3937e9f6e3121aabafbd1d25
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2023-12-08 13:00:50 -0600

    Remove dict(...) wrapper

--- tap_postgres/client.py
+++ tap_postgres/client.py
@@ -283,7 +283,7 @@ class PostgresStream(SQLStream):
 
         with self.connector._connect() as con:
             for row in con.execute(query).mappings().all():
-                yield dict(row)
+                yield row
 
 
 class PostgresLogBasedStream(SQLStream):
